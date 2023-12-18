db5cd146d62c5ebb53b5197459223dc1e2d67a53
Bravo Yeung <yangle20081982@163.com>
2023-12-18 07:26:28 +0000

    Add sample C# test code.

--- Program.cs
+++ Program.cs
@@ -1,34 +1,19 @@
+using System.Globalization;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+// Add test code here
+DateTime dateValue;
+if (DateTime.TryParse("2013/07/22", new CultureInfo("zh-TW"), DateTimeStyles.None, out dateValue))
+{
+    Console.WriteLine(dateValue.ToString());
+}
+else
+{
+    Console.WriteLine("fail");
+}
+
 app.Urls.Add("http://localhost:5000");
-
 app.MapGet("/", () => "Hello World!");
-
-app.MapGet("/{cityName}/weather", GetWeatherByCity);
-
 app.Run();
-
-
-Weather GetWeatherByCity(string cityName)
-{
-    app.Logger.LogInformation($"Weather requested for {cityName}.");
-    var weather = new Weather(cityName);
-    return weather;
-}
-
-public record Weather
-{
-    public string City { get; set; }
-
-    public Weather(string city)
-    {
-        City = city;
-        Conditions = "Cloudy";
-        // Temperature here is in celsius degrees, hence the 0-40 range.
-        Temperature = new Random().Next(0,40).ToString();
-    }
-
-    public string Conditions { get; set; }
-    public string Temperature { get; set; }
-}
